bb1ce568a3e3f7ab04d851128e1791dd25524f46
Freimans <freimans@post.bgu.ac.il>
2020-11-21 13:04:32 +0200

    Added OGAM_Run class

---
+++ MinCostMaxFlow/OGAM_Run.cs
@@ -0,0 +1,27 @@
+﻿using Google.OrTools.Graph;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace CPF_experiment
+{
+    class OGAM_Run
+    {
+        public long solve(MAM_ProblemInstance instance, Move goalstate)
+        {
+            CFMAM_MCMF_Reducer reducer = new CFMAM_MCMF_Reducer(instance, goalstate);
+            reducer.reduce();
+            MinCostMaxFlow mcmfSolver = new MinCostMaxFlow(reducer.outputProblem);
+            Stopwatch timer = Stopwatch.StartNew();
+            MinCostFlow solution = mcmfSolver.SolveMinCostFlow();
+            timer.Stop();
+            if (solution != null)
+            {
+                reducer.GetCFMAMSolution(solution, timer.ElapsedMilliseconds);
+                return solution.OptimalCost();
+            }
+            return -1;
+        }
+    }
+}
